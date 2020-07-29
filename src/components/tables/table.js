85614ee8d43e54a899af0c08ec52776e893cc7e7
Tom Gallagher <thomasgallagher101@gmail.com>
2020-07-29 14:31:08 +0100

    small change to table css

--- src/components/tables/table.js
+++ src/components/tables/table.js
@@ -2,10 +2,12 @@ import React, { useMemo } from 'react';
 import { Table, Menu, Icon, Select } from 'semantic-ui-react';
 import { useTable, useSortBy, usePagination } from 'react-table';
 
+import './table.css';
+
 export const SemanticTable = ({ headers, dataset, ...props }) => {
     //make sure we memo-ize the incoming data so we can work it
-    const columns = useMemo(() => headers, []);
-    const data = useMemo(() => dataset, []);
+    const columns = useMemo(() => headers, [headers]);
+    const data = useMemo(() => dataset, [dataset]);
 
     // Use the state and functions returned from useTable to build your UI
     const {
@@ -32,135 +34,145 @@ export const SemanticTable = ({ headers, dataset, ...props }) => {
     );
 
     return (
-        // apply the table props
-        <Table {...getTableProps(props)}>
-            {/* build the table header */}
-            <Table.Header>
-                {
-                    // Loop over the header rows
-                    headerGroups.map((headerGroup) => (
-                        // Apply the header row props
-                        <Table.Row {...headerGroup.getHeaderGroupProps()}>
-                            {
-                                // Loop over the headers in each row
-                                headerGroup.headers.map((column) => (
-                                    // Apply the header cell props
+        /* This is required to make the table full-width */
+        <div className='tableContainer'>
+            {/* This will make the table scrollable when it gets too small */}
+            <div className='tableWrapper'>
+                {/* apply the table props */}
+                <Table {...getTableProps(props)} style={{ width: '100%' }}>
+                    {/* build the table header */}
+                    <Table.Header>
+                        {
+                            // Loop over the header rows
+                            headerGroups.map((headerGroup) => (
+                                // Apply the header row props
+                                <Table.Row
+                                    {...headerGroup.getHeaderGroupProps()}
+                                >
+                                    {
+                                        // Loop over the headers in each row
+                                        headerGroup.headers.map((column) => (
+                                            // Apply the header cell props
 
-                                    <Table.HeaderCell
-                                        {...column.getHeaderProps(
-                                            // Add the sorting props to control sorting.
-                                            column.getSortByToggleProps()
-                                        )}
-                                        /* Add a sort direction indicator, using the sort properties of the column and the semantic ui sorted property */
-                                        sorted={
-                                            column.isSorted
-                                                ? column.isSortedDesc
-                                                    ? 'descending'
-                                                    : 'ascending'
-                                                : null
-                                        }
-                                    >
-                                        {
-                                            // Render the header
-                                            column.render('Header')
-                                        }
-                                    </Table.HeaderCell>
-                                ))
-                            }
-                        </Table.Row>
-                    ))
-                }
-            </Table.Header>
-
-            {/* Apply the table body props */}
-            <Table.Body {...getTableBodyProps()}>
-                {
-                    // Loop over the table rows
-                    page.map((row) => {
-                        // Prepare the row for display
-                        prepareRow(row);
-                        return (
-                            // Apply the row props
-                            <Table.Row {...row.getRowProps()}>
-                                {
-                                    // Loop over the rows cells
-                                    row.cells.map((cell) => {
-                                        // Apply the cell props
-                                        return (
-                                            <Table.Cell
-                                                {...cell.getCellProps()}
+                                            <Table.HeaderCell
+                                                {...column.getHeaderProps(
+                                                    // Add the sorting props to control sorting.
+                                                    column.getSortByToggleProps()
+                                                )}
+                                                /* Add a sort direction indicator, using the sort properties of the column and the semantic ui sorted property */
+                                                sorted={
+                                                    column.isSorted
+                                                        ? column.isSortedDesc
+                                                            ? 'descending'
+                                                            : 'ascending'
+                                                        : null
+                                                }
                                             >
                                                 {
-                                                    // Render the cell contents
-                                                    cell.render('Cell')
+                                                    // Render the header
+                                                    column.render('Header')
                                                 }
-                                            </Table.Cell>
-                                        );
-                                    })
-                                }
-                            </Table.Row>
-                        );
-                    })
-                }
-            </Table.Body>
+                                            </Table.HeaderCell>
+                                        ))
+                                    }
+                                </Table.Row>
+                            ))
+                        }
+                    </Table.Header>
 
-            <Table.Footer>
-                <Table.Row>
-                    <Table.HeaderCell
-                        colSpan={visibleColumns.length.toString()}
-                    >
-                        <Menu floated='right' pagination>
-                            <Menu.Item
-                                as='a'
-                                icon
-                                onClick={() => gotoPage(0)}
-                                disabled={!canPreviousPage}
-                            >
-                                <Icon name='chevron left' />
-                                <Icon name='chevron left' />
-                            </Menu.Item>
-                            <Menu.Item
-                                as='a'
-                                icon
-                                onClick={() => previousPage()}
-                                disabled={!canPreviousPage}
-                            >
-                                <Icon name='chevron left' />
-                            </Menu.Item>
-                            <Menu.Item
-                                as='a'
-                                icon
-                                onClick={() => nextPage()}
-                                disabled={!canNextPage}
-                            >
-                                <Icon name='chevron right' />
-                            </Menu.Item>
-                            <Menu.Item
-                                as='a'
-                                icon
-                                onClick={() => gotoPage(pageCount - 1)}
-                                disabled={!canNextPage}
-                            >
-                                <Icon name='chevron right' />
-                                <Icon name='chevron right' />
-                            </Menu.Item>
-                            <Menu.Item as='a'>
-                                Page {pageIndex + 1} of {pageOptions.length}
-                            </Menu.Item>
+                    {/* Apply the table body props */}
+                    <Table.Body {...getTableBodyProps()}>
+                        {
+                            // Loop over the table rows
+                            page.map((row) => {
+                                // Prepare the row for display
+                                prepareRow(row);
+                                return (
+                                    // Apply the row props
+                                    <Table.Row {...row.getRowProps()}>
+                                        {
+                                            // Loop over the rows cells
+                                            row.cells.map((cell) => {
+                                                // Apply the cell props
+                                                return (
+                                                    <Table.Cell
+                                                        {...cell.getCellProps()}
+                                                    >
+                                                        {
+                                                            // Render the cell contents
+                                                            cell.render('Cell')
+                                                        }
+                                                    </Table.Cell>
+                                                );
+                                            })
+                                        }
+                                    </Table.Row>
+                                );
+                            })
+                        }
+                    </Table.Body>
 
-                            <Select
-                                item
-                                text={`Show ${pageSize}`}
-                                onChange={(e, { value }) => {
-                                    setPageSize(Number(value));
-                                }}
-                                options={paginationOptions}
-                            />
-                        </Menu>
-                    </Table.HeaderCell>
-                </Table.Row>
-            </Table.Footer>
-        </Table>
+                    <Table.Footer>
+                        <Table.Row>
+                            <Table.HeaderCell
+                                colSpan={visibleColumns.length.toString()}
+                            >
+                                <Menu floated='right' pagination>
+                                    <Menu.Item
+                                        as='a'
+                                        icon
+                                        onClick={() => gotoPage(0)}
+                                        disabled={!canPreviousPage}
+                                    >
+                                        <Icon name='chevron left' />
+                                        <Icon name='chevron left' />
+                                    </Menu.Item>
+                                    <Menu.Item
+                                        as='a'
+                                        icon
+                                        onClick={() => previousPage()}
+                                        disabled={!canPreviousPage}
+                                    >
+                                        <Icon name='chevron left' />
+                                    </Menu.Item>
+                                    <Menu.Item
+                                        as='a'
+                                        icon
+                                        onClick={() => nextPage()}
+                                        disabled={!canNextPage}
+                                    >
+                                        <Icon name='chevron right' />
+                                    </Menu.Item>
+                                    <Menu.Item
+                                        as='a'
+                                        icon
+                                        onClick={() => gotoPage(pageCount - 1)}
+                                        disabled={!canNextPage}
+                                    >
+                                        <Icon name='chevron right' />
+                                        <Icon name='chevron right' />
+                                    </Menu.Item>
+                                    <Menu.Item as='a'>
+                                        Page {pageIndex + 1} of{' '}
+                                        {pageOptions.length}
+                                    </Menu.Item>
+
+                                    <Select
+                                        item
+                                        upward={true}
+                                        text={`Show ${pageSize}`}
+                                        onChange={(e, { value }) => {
+                                            setPageSize(Number(value));
+                                        }}
+                                        options={paginationOptions}
+                                    />
+                                </Menu>
+                            </Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Footer>
+                </Table>
+            </div>
+        </div>
     );
 };
 
@@ -207,8 +219,4 @@ SemanticTable.defaultProps = {
     structured: false,
     //A table can adjust its text alignment.
     textAlign: 'left',
-    //A table can specify how it stacks table content responsively.
-    unstackable: false,
-    //A table can adjust its text alignment, bottom, middle, top
-    verticalAlign: 'middle',
 };
