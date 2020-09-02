7abff41d15e9f8d675e886bd657e8edf59011d06
Tom Gallagher <thomasgallagher101@gmail.com>
2020-09-02 12:22:35 +0100

    added array sort with precedence for objects

--- src/utils/arrayFunctions.js
+++ src/utils/arrayFunctions.js
@@ -37,3 +37,64 @@ export const TotalMegaBytes = (acc, value, index, array) => {
     //otherwise we are still interating and we just return the running total
     return calculatedValue;
 };
+
+//SORT FUNCTION FOR OBJECTS WHERE YOU NEED TO PRIORITISE CERTAIN FIELDS
+//Call function like so
+/*
+    const sorted = testArray.sort((a, b) =>
+        sortWithFieldPrecedence(a, b, ["age", "lastName", "status"])
+    );
+*/
+//where the array contains the fields and the order you want to sort
+
+export const sortWithFieldPrecedence = (a, b, precedenceArray) => {
+    //map precedence array strings to object with type
+    const precedences = precedenceArray.map(
+        //this enables us to examine the properties of a and b
+        (str) => ({ key: str, type: typeof a[str] })
+    );
+    //error handler in cases where the precedence array fields not present in either of objects to sort
+    if (precedences.some((item) => !Object.keys(a).includes(item.key) || !Object.keys(b).includes(item.key))) {
+        console.log('Precedence fields unmatched in array object property keys');
+        return 0;
+    }
+    //in situations of equality of all fields we return 0, so no sorting
+    if (!precedences.some((item) => a[item.key] !== b[item.key])) {
+        console.log('Matched by all fields');
+        return 0;
+    }
+    //otherwise we need to test each item in the precedence array
+    //we use array reduce to return the sorting indicator for all precedences
+    return precedences.reduce((accumulator, currentValue, index) => {
+        //at the start we have no sort signal and also in the loop when equality
+        if (accumulator === 0) {
+            //we need a report on sorting that has occurred beyond the first criterion
+            if (index > 0) {
+                const objA = JSON.stringify(a);
+                const objB = JSON.stringify(b);
+                console.log(`Calculating Precedence at layer ${index}: ${currentValue.key}`);
+                console.log(objA);
+                console.log(objB);
+            }
+            //otherwise we need to vary according to the type of sort
+            switch (currentValue.type) {
+                case 'string':
+                    const aString = a[currentValue.key].toLowerCase();
+                    const bString = b[currentValue.key].toLowerCase();
+                    //To see whether a string is greater than another,
+                    //JavaScript uses the so-called “dictionary” or “lexicographical” order.
+                    if (aString < bString) accumulator = -1;
+                    if (aString > bString) accumulator = 1;
+                    if (aString === bString) accumulator = 0;
+                    break;
+                case 'number':
+                    //with numbers we can just return the first minus the second
+                    accumulator = a[currentValue.key] - b[currentValue.key];
+                    break;
+                default:
+                    console.log('Unrecognised key type');
+            }
+        }
+        return accumulator;
+    }, 0);
+};
